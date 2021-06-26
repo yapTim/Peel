a217264b67b05df00bbbef9fee123f6d827cb5a4
Robert Yap <yap.rtim@gmail.com>
2021-06-27 01:48:52 +0800

    Setup snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,9 @@
+export default {
+    mount: {
+        public: { url: '/', static: true },
+        src: { url: '/dist' },
+    },
+    plugins: [
+        '@snowpack/plugin-typescript',
+    ]
+};
