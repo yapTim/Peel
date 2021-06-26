9448db233f07e598b07b886525cd10ff269ca7fd
Robert Yap <yap.rtim@gmail.com>
2021-06-27 02:00:11 +0800

    Add sample firebase code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const x = 'Hello World!!!!';
-console.log(x);
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth()
+signInWithEmailAndPassword(auth, 'yap.rtim@gmail.com', 'password')
+    .then((userCredential: any) => {
+        console.log('SUCCESS')
+        console.log(userCredential)
+    })
+    .catch((error: any) => {
+        console.log('This is an error', error);
+    });
---
+++ src/initFirebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCMdzZYfCpwR1KLg3-hPPlG3WCUNT-TA58",
+    authDomain: "peel-f61da.firebaseapp.com",
+    projectId: "peel-f61da",
+    storageBucket: "peel-f61da.appspot.com",
+    messagingSenderId: "8536704246",
+    appId: "1:8536704246:web:d5321de3f354f7555d98d7"
+};
+
+initializeApp(firebaseConfig);
