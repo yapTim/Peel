ce5e0d279e6bd4854b68cc6921f85a6218d3fc94
renzo pc <renzo@DESKTOP-3LMM4ON.localdomain>
2021-06-28 21:12:59 +0800

    Add firebase functionality to skeletal login page.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
-const auth = getAuth()
-signInWithEmailAndPassword(auth, 'yap.rtim@gmail.com', 'password')
-    .then((userCredential: any) => {
-        console.log('SUCCESS')
-        console.log(userCredential)
-    })
-    .catch((error: any) => {
-        console.log('This is an error', error);
-    });
+const loginUsername: HTMLInputElement = document.querySelector('#loginUsername')
+const loginPassword: HTMLInputElement = document.querySelector('#loginPassword')
+const loginBtn = document.querySelector('#loginBtn')
+
+loginBtn.addEventListener('click', () => {
+    console.log(loginUsername.value)
+    console.log(loginPassword.value)
+    
+    const auth = getAuth()
+    signInWithEmailAndPassword(auth, loginUsername.value, loginPassword.value)
+        .then((userCredential: any) => {
+            console.log('SUCCESS')
+            console.log(userCredential)
+        })
+        .catch((error: any) => {
+            console.log('This is an error', error);
+        });
+})
