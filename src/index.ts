e4b3a10f1afc2256d635f89f0afd7cfa7ae3632c
renzo pc <renzo@DESKTOP-3LMM4ON.localdomain>
2021-06-30 17:56:04 +0800

    Add design to Login Page.
    Add pop-up modal with message ("Success!" or "Wrong/Empty e-mail or password.") corresponding to firebase login auth status.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const loginUsername: HTMLInputElement = document.querySelector('#loginUsername')
 const loginPassword: HTMLInputElement = document.querySelector('#loginPassword')
+const loginState: HTMLElement = document.querySelector('#loginState')
 const loginBtn = document.querySelector('#loginBtn')
+const closeLoginBtn = document.querySelector('#closeLoginBtn')
+
 
 loginBtn.addEventListener('click', () => {
     console.log(loginUsername.value)
@@ -11,10 +14,18 @@ loginBtn.addEventListener('click', () => {
     const auth = getAuth()
     signInWithEmailAndPassword(auth, loginUsername.value, loginPassword.value)
         .then((userCredential: any) => {
-            console.log('SUCCESS')
             console.log(userCredential)
+            loginState.innerText = "Success!"
+            closeLoginBtn.addEventListener('click', () => {
+                window.location.href = "inventory.html"
+                })
         })
         .catch((error: any) => {
             console.log('This is an error', error);
+            loginState.innerText = "Empty or incorrect email/password!"
+            closeLoginBtn.addEventListener('click', () => {
+                setTimeout(() => loginState.innerText = "Loading...", 500)
+                })
         });
 })
+
